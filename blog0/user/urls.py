36ea3e06e48b3f8968a0b8a331498688fa88bb7e
huitong-070 <huitong-070@huitong-070deMacBook-Pro.local>
2018-08-10 17:35:14 +0800

    wayne django second simple user authenticate

--- blog0/user/urls.py
+++ blog0/user/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.homepage),
     url(r'^test', views.reg),
+    url(r'^login$', views.login),
+    url(r'^atest$', views.test),
 ]
--- blog0/user/views.py
+++ blog0/user/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedire
 from .models import User
 import simplejson
 import jwt, datetime
-from blog0 import settings
+import bcrypt
+from django.conf import settings
 
 # Create your views here.
 
@@ -38,7 +39,8 @@ def reg(request):
         user = User()
         user.email = email
         user.name = name
-        user.password = password
+        user.password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
         try:
             user.save()
             print('*' * 25)
@@ -49,3 +51,68 @@ def reg(request):
     except Exception as e:
         print(e)
         return HttpResponseBadRequest()
+
+
+def login(request):
+    try:
+        payload = simplejson.loads(request.body)
+        email = payload['email']
+        password = payload['password']
+
+        # 验证邮箱是否存在，如果存在，就查看密码
+        user = User.objects.filter(email=email).first()
+        if not user:  # 表示查无此人
+            return HttpResponseBadRequest()  #暂时先返回有问题的请求
+        if not bcrypt.checkpw(password.encode(), user.password.encode()):
+            return HttpResponseBadRequest()  #说明密码不对，暂时返回400
+
+        return JsonResponse({
+            'user': {
+                'user_id': user.id,
+                'name': user.name,
+                'email': user.email
+            },
+            'token': gen_token(user.id)
+        })  # 200
+
+    except Exception as e:
+        print(e)
+        return HttpResponseBadRequest()
+
+AUTH_EXPIRE = 60 * 60 * 5
+
+
+def authenticate(view):
+    def wrapper(request):
+        #将jwt放入header中
+        #print(list(request.META.items()))
+        token = request.META.get('HTTP_JWT')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+            #  取出payload  是 一个header中的token
+            print(payload)
+            print(payload['timestamp'])
+            # 考虑 过期时间
+            if (datetime.datetime.now().timestamp() - payload['timestamp']) > AUTH_EXPIRE:
+                return HttpResponse(status=401)
+            # 到这里，说明用户已经算是个合法用户
+            user_id = payload['user_id']
+
+            #查一次数据库
+            user = User.objects.get(pk=user_id)
+
+            request.user = user
+
+            return view(request)
+
+            #return HttpResponse('2oo!')
+        except Exception as e:
+            print(e)
+            return HttpResponse(status=401)
+    return wrapper
+
+
+@authenticate
+def test(request):
+    return HttpResponse(b'ok')
+
